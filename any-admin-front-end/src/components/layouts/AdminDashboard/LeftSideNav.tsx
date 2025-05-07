f341de5f4932766ad39b979375cd0c33e1c1fb2c
antoniocodemaster <antoniocodemaster@gmail.com>
2025-05-07 14:16:53 -0600

    Improving active menu

--- any-admin-front-end/src/components/layouts/AdminDashboard/LeftSideNav.tsx
+++ any-admin-front-end/src/components/layouts/AdminDashboard/LeftSideNav.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import Title from '../../typography/Title';
 import {
   HomeIcon,
@@ -11,19 +11,33 @@ import useAuthState from '../../../lib/states/AuthState';
 const LeftSideNav = () => {
   const navigate = useNavigate();
   const { logout } = useAuthState();
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  const getLinkClassName = (path: string) => {
+    return `dashboard-left-nav-link ${
+      pathname === path ? 'active' : ''
+    }`;
+  };
 
   return (
     <div className="w-52 h-screen bg-white">
       <Title title="Admin Dashboard" className="p-4" style="SectionTitle" />
-      <Link className="dashboard-left-nav-link" to="/admin-dashboard">
+      <Link
+        className={getLinkClassName('/admin-dashboard')}
+        to="/admin-dashboard"
+      >
         <Bars3Icon className="w-6 h-6" />
         Dashboard
       </Link>
-      <Link className="dashboard-left-nav-link" to="/admin-dashboard/contacts">
+      <Link
+        className={getLinkClassName('/admin-dashboard/contacts')}
+        to="/admin-dashboard/contacts"
+      >
         <UserGroupIcon className="w-6 h-6" />
         Contacts
       </Link>
-      <Link className="dashboard-left-nav-link" to="/">
+      <Link className={getLinkClassName('/')} to="/">
         <HomeIcon className="w-6 h-6" />
         Landing Page
       </Link>
